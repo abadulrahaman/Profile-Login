acbc658190516facdf3b6b4473010467c668328b
abadulrahaman <abadulrahamanrain@gmail.com>
2024-10-29 10:46:40 +0545

    user profile func added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,26 +1,49 @@
-import { Data } from "../../data";
-import Card from "./Card";
-import DataModal from "./DataModal";
+import { useState } from 'react'
+import { Data } from '../../data'
+import Card from './Card'
+import DataModal from './DataModal'
 
 const Body = () => {
-    
-    const onSubmit = (data) => {
-        console.log(data);
-    };
+  const [users, setUsers] = useState(Data)
+  const [showModal, setShowModal] = useState(false)
+  const [id, setId] = useState('')
 
-    return(
-        <div className="flex flex-wrap gap-4  items-center">
-            <DataModal onSubmit={onSubmit}/>
+  const onSubmit = (data) => {
+    users[id].name = data.name
+    users[id].email = data.email
+    users[id].phone = data.phone
+    users[id].url = data.website
+    users[id].imgurl = data.imageurl
+    setShowModal(false)
+    console.log(data)
+  }
 
-            {
-                Data.map((val) => (
-                <Card value = {val}/>
+  const handleShowModal = (status) => {
+    setShowModal(status)
+  }
 
-                
-            ))
-            }
-        </div>
-    );
-};
+  const handleId = (id) => {
+    setId(id)
+  }
 
-export default Body;
+  return (
+    <div className="flex flex-wrap gap-4  items-center">
+      {showModal ? (
+        <DataModal handleShowModal={handleShowModal} onSubmit={onSubmit} />
+      ) : (
+        ''
+      )}
+
+      {users.map((val) => (
+        <Card
+          value={val}
+          handleId={handleId}
+          key={val.id}
+          handleShowModal={handleShowModal}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default Body
--- src/components/DataModal.jsx
+++ src/components/DataModal.jsx
@@ -1,49 +1,59 @@
-import { useForm } from "react-hook-form";
-const  DataModal = (props) => {  
-    const {onSubmit} = props;
+import { useForm } from 'react-hook-form'
+const DataModal = (props) => {
+  const { onSubmit, handleShowModal } = props
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  // destructure
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
 
-
-    return(
-        <div className="bg-white h-screen w-full fixed bg-opacity-65 top-0 flex justify-center items-center">
-            <form 
-            onSubmit={handleSubmit(onSubmit)}
-
-            className="h-fit w-[350px] border-2 bg-white p-4 flex flex-col gap-4">
-
-                <div className="flex justify-between">
-                    <p className="text-xl text-black ">Basic Modal</p>
-                    <p>X</p>
-                </div>
-
-                <div className="flex justify-between">
-                    <p>Name:</p>
-                    <input className="border-2" type="text" {...register("name")}/>
-                </div>
-
-                
-                <div className="flex justify-between">
-                    <p>Email:</p>
-                    <input className="border-2" type="email" {...register("email")}/>
-                </div>
-                <div className="flex justify-between">
-                    <p>Phone:</p>
-                    <input className="border-2" type="number" {...register("phone")}/>
-                </div>
-                <div className="flex justify-between">
-                    <p>Website:</p>
-                    <input className="border-2" type="url" {...register("website")}/>
-                </div>
-                <div className="flex justify-between">
-                    <p>Imageurl:</p>
-                    <input className="border-2" type="url" {...register("imageurl")}/>
-                </div>
-                <button className="bg-blue-700 rounded-lg p-4" type="submit">Submit</button>
-            </form>
-
+  return (
+    <div className="bg-white h-screen w-full fixed bg-opacity-65 top-0 flex justify-center items-center">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="h-fit w-[350px] border-2 bg-white p-4 flex flex-col gap-4"
+      >
+        <div className="flex justify-between">
+          <p className="text-xl text-black ">Basic Modal</p>
+          <p
+            onClick={() => {
+              handleShowModal(false)
+            }}
+          >
+            X
+          </p>
         </div>
-    );
-};
 
-export default DataModal;
+        <div className="flex justify-between">
+          <p>Name:</p>
+          <input className="border-2" type="text" {...register('name')} />
+        </div>
+
+        <div className="flex justify-between">
+          <p>Email:</p>
+          <input className="border-2" type="email" {...register('email')} />
+        </div>
+        <div className="flex justify-between">
+          <p>Phone:</p>
+          <input className="border-2" type="number" {...register('phone')} />
+        </div>
+        <div className="flex justify-between">
+          <p>Website:</p>
+          <input className="border-2" type="url" {...register('website')} />
+        </div>
+        <div className="flex justify-between">
+          <p>Imageurl:</p>
+          <input className="border-2" type="url" {...register('imageurl')} />
+        </div>
+        <button className="bg-blue-700 rounded-lg p-4" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default DataModal
